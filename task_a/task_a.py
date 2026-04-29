a74cbea5cab20d0389763870da909257c09c638f
Zuzana Černohousová <cernohz@natur.cuni.cz>
2026-04-29 16:07:38 +0200

    create a failing test

--- task_a/task_a.py
+++ task_a/task_a.py
@@ -19,6 +19,7 @@ def test_fib() -> None:
     assert fib(0) == 0
     assert fib(1) == 1
     assert fib(2) == 1
+    assert fib(1) == 23
     assert fib(3) == 2
     assert fib(5) == 5
     assert fib(10) == 55
